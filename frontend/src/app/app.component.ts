ab9ad5ed0826f6d32779101a2affb42f3c1dca6f
Owen Lin <owen4work@gmail.com>
2022-06-08 17:23:09 +0800

    load users.json

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import * as data from './models/users.json';
+import { User } from './models/user.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  users: User[] = (data as any).default;
+
+  constructor() {
+    console.log(this.users)
+  }
 }
